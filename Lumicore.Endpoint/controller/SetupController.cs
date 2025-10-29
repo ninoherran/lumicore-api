34c71cac855764c7008c1def46f0cdf1bb3f4949
Nino Herran <dev@ninoherran.fr>
2025-10-29 22:20:18 +0100

    Initial commit

---
+++ Lumicore.Endpoint/controller/SetupController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lumicore.Endpoint.controller;
+
+[ApiController]
+[Route("[controller]")]
+public class SetupController : ControllerBase
+{
+    [HttpGet("is-init")]
+    public IActionResult IsInit()
+    {
+        return BadRequest();
+    }
+}
